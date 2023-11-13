db69c1edfac7913c1cb03fe6b08bd770b5f02c0d
mamun-prodhan <mamunpr9@gmail.com>
2023-11-13 18:38:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+    </>
+  );
+}
+
+export default App;
