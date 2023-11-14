d2cf68178c23e463a7aa33fc8181effbb905f9a5
mamun-prodhan <mamunpr9@gmail.com>
2023-11-14 17:23:53 +0600

    parallax added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,7 +9,24 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <h2>This is menu</h2>
+      <Cover
+        img={menuImg}
+        title={"our menu"}
+        description={"Would you like to try a dish?"}
+      ></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover
+        img={menuImg}
+        title={"our menu"}
+        description={"Would you like to try a dish?"}
+      ></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover
+        img={menuImg}
+        title={"our menu"}
+        description={"Would you like to try a dish?"}
+      ></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
