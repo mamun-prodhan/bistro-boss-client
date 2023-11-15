a30a577cac854501fc4848235d93a114b5d11414
mamun-prodhan <mamunpr9@gmail.com>
2023-11-16 01:48:20 +0600

    food order page polished

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -8,11 +8,13 @@ const FoodCard = ({ item }) => {
       <p className="absolute right-5 top-5 me-5 bg-slate-900 text-white px-5 py-3">
         ${price}
       </p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to Cart</button>
+        <div className="card-actions justify-center">
+          <button className=" btn bg-slate-200 border-orange-500 btn-outline border-0 border-b-4 ">
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
