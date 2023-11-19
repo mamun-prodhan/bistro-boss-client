d96987b460953bd5fbf30e7630f88f8f5019a423
mamun-prodhan <mamunpr9@gmail.com>
2023-11-19 10:23:36 +0600

    cart total added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -3,6 +3,7 @@ import {
   FaCalendar,
   FaHome,
   FaList,
+  FaSearch,
   FaShoppingCart,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
@@ -38,10 +39,21 @@ const Dashboard = () => {
               <FaList></FaList> My Bookings
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome>Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <FaSearch></FaSearch>Menu
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* Dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
