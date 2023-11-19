576a9fe7230b0428e61975993a6472f2cc6a447a
mamun-prodhan <mamunpr9@gmail.com>
2023-11-19 11:20:54 +0600

    cart display and delete implemented

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -7,8 +7,10 @@ import {
   FaShoppingCart,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+  const [cart] = useCart();
   return (
     <div className="flex">
       {/* Dashboard side bar */}
@@ -26,7 +28,7 @@ const Dashboard = () => {
           </li>
           <li>
             <NavLink to="/dashboard/cart">
-              <FaShoppingCart></FaShoppingCart> My Cart
+              <FaShoppingCart></FaShoppingCart> My Cart ({cart.length})
             </NavLink>
           </li>
           <li>
